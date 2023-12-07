1f5a53108c46c555c1bcdab960b5d621ca798da9
Paweł Mstowski <pawel.mstowski@gmail.com>
2023-12-07 18:49:12 +0100

    Add basic config for my convinience

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open terminal on the bottom
+vim.keymap.set("n", "<leader>tr", ":belowright 20 split | terminal<CR>", { desc = "[T]e[R]minal" })
+vim.keymap.set("n", "<leader>nh", ":noh<CR>", { desc = "[N]o [H]ighlight" })
